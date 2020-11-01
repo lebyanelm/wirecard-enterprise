daae0597b9eecf7d8c75fde7f3336d74be514087
Libby <lebyane.lm@gmail.com>
2020-11-01 10:49:33 +0200

    Merged types into one file

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-import { TDSLookupRequestOptions } from './types/TDSLookupRequestOptions';
-import { TDSLookupResponse } from './types/TDSLookupResponse';
-
-declare class WirecardTransactions {
-    tdsLookup(options: TDSLookupRequestOptions): Promise<TDSLookupResponse>;
-}
